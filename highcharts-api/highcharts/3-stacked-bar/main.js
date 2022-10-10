e014e114985cb65c311b696d546d62b34d7dc339
Kamil Kubik <kamilkubik@Kamils-MBP.localdomain>
2022-10-10 12:18:12 +0200

    Apply initial code review changes

--- highcharts-api/highcharts/3-stacked-bar/main.js
+++ highcharts-api/highcharts/3-stacked-bar/main.js
@@ -4,36 +4,48 @@ Highcharts.chart("container", {
     marginTop: 50,
     events: {
       load() {
-        this.renderer.text("Issue", 0, 35).add();
-        this.renderer.text("Record Count", this.plotLeft, 35).add();
-        this.renderer.text("Action", this.plotWidth, 35).add();
+        const chart = this;
 
-        const buttonStyles = {
-          fill: "#fff",
-          stroke: "blue",
-          "stroke-width": 2,
-        };
+        chart.renderer.text("Issue", 0, 35).add();
+        chart.renderer.text("Record Count", chart.plotLeft, 35).add();
+
+        const actionLabel = chart.renderer
+          .text("Action", chart.plotWidth, 35)
+          .add();
+        chart.actionLabel = actionLabel;
 
         const buttons = Array.from({ length: 4 }, (_, index) =>
-          this.renderer
+          chart.renderer
             .button(
               "How to fix",
-              this.plotWidth,
-              this.axes[0].getThreshold(index) + this.marginBottom,
+              chart.plotWidth,
+              chart.axes[0].getThreshold(index) + chart.marginBottom,
               () => null,
-              buttonStyles,
-              buttonStyles
+              {
+                fill: "#fff",
+                stroke: "blue",
+                "stroke-width": 2,
+              }
             )
-            .attr(buttonStyles)
             .add()
         );
 
-        this.customButtons = buttons;
+        chart.customButtons = buttons;
       },
       render() {
-        const customButtons = this.customButtons;
+        let tTick = 0;
 
-        if (Array.isArray(customButtons) && customButtons.length > 0) {
+        Object.values(this.xAxis[0].ticks).forEach((tick) => {
+          console.info(tick);
+          tTick = tick.gridLine.renderer.width;
+
+          tick.gridLine.pathArray[0][1] = this.spacingBox.x;
+        });
+
+        const customButtons = this.customButtons;
+        const actionLabel = this.actionLabel;
+
+        if (customButtons) {
           customButtons.map((button, index) =>
             button
               .attr(0)
@@ -45,11 +57,15 @@ Highcharts.chart("container", {
               )
           );
         }
+
+        if (actionLabel) {
+          actionLabel.attr({ x: this.plotWidth });
+        }
       },
     },
   },
   title: {
-    text: undefined,
+    text: "",
   },
   xAxis: {
     categories: ["Data", "Emails", "Duplicates", "Support"],
@@ -58,6 +74,7 @@ Highcharts.chart("container", {
   },
   yAxis: {
     min: 0,
+    max: 350,
     tickInterval: 50,
     gridLineWidth: 0,
     title: {
@@ -68,9 +85,7 @@ Highcharts.chart("container", {
       style: {
         fontWeight: "bold",
       },
-      formatter() {
-        return `${this.total} K`;
-      },
+      format: "${total} K",
     },
   },
   legend: {
