e18d90d4cb9366a9e694c0cbc5d7ecc7fcfc3eb6
Kamil Kubik <kamilkubik@Kamils-MBP.localdomain>
2022-10-11 15:36:18 +0200

    Applied exercise 6 finished version

--- highcharts-api/highcharts/6-visible-points-zoom/main.js
+++ highcharts-api/highcharts/6-visible-points-zoom/main.js
@@ -6,29 +6,67 @@ Highcharts.chart('container', {
         zooming: {
             type: 'xy'
         },
+        animation: true,
         events: {
-            load() {
-                const chart = this,
-                    countLabel = chart.renderer.text('Visible count: 100', chart.plotLeft, chart.plotHeight + chart.plotTop + 52).add();
-
-                chart.countLabel = countLabel;
-            },
             render() {
                 const chart = this,
                     countLabel = chart.countLabel,
                     points = chart.series[0].points,
-                    visiblePointsCount = points.filter((point) => point.isInside).length;
+                    visiblePoints = points.filter((point) => point.isInside),
+                    visiblePointsTexts = chart.visiblePointsTexts,
+                    visibleCircles = chart.visibleCircles;
 
                 if (countLabel) {
-                    countLabel.attr({ text: `Visible count: ${visiblePointsCount}` })
-                }
+                    countLabel.attr({ text: `Visible count: ${visiblePoints.length}` })
+                } else {
+                    const renderedCountLabel = chart.renderer
+                        .text(
+                          'Visible count: 100',
+                          chart.plotLeft,
+                          chart.plotHeight + chart.plotTop + 52
+                        )
+                        .add();
 
-                console.info(countLabel);
+                    chart.countLabel = renderedCountLabel;
+                };
+
+                if (visiblePointsTexts?.length > 0) {
+                    visiblePointsTexts.forEach((visiblePointText) => visiblePointText.destroy());
+                };
+
+                if (visibleCircles?.length > 0) {
+                    visibleCircles.forEach((visibleCircle) => visibleCircle.destroy());
+                };
+
+                console.info(visiblePoints);
+
+                const highestVisiblePoint = Math.max(...visiblePoints.map((visiblePoint) => visiblePoint.options.y));
+                const renderedVisiblePoints = visiblePoints.filter((visiblePoint) => visiblePoint.options.y === highestVisiblePoint);
+                const renderedVisibleTextes = renderedVisiblePoints.map((visiblePoint) => 
+                    chart.renderer
+                        .text(
+                          visiblePoint.options.y.toString(),
+                          chart.plotLeft + visiblePoint.plotX, 
+                          visiblePoint.plotY + chart.plotTop - 10
+                        )
+                        .attr({ fill: 'red', 'text-anchor': 'middle' })
+                        .add()
+                );
+                const renderedVisibleCircles = renderedVisiblePoints.map((visiblePoint) =>
+                    chart.renderer
+                        .circle(
+                          visiblePoint.plotX + chart.plotLeft, 
+                          chart.plotHeight + chart.plotTop, 3
+                        )
+                        .attr({ fill: 'red' })
+                        .add()
+                        .toFront()   
+                );     
+                
+                chart.visiblePointsTexts = renderedVisibleTextes;
+                chart.visibleCircles = renderedVisibleCircles;
             }
         }
-    },
-    credits: {
-        enabled: false
     },
     title: {
         text: 'Chart title'
@@ -38,9 +76,8 @@ Highcharts.chart('container', {
             text: 'Values'
         }
     },
-    xAxis: {},
     series: [{
-        name: 'Lines',
+        name: 'Series 1',
         data: generateSeries()
     }],
 });
