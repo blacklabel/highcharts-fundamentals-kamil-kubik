b6ecd97ff3e1ce849bcb0f251cf7962775758509
Kamil Kubik <kamilkubik@Kamils-MBP.localdomain>
2022-10-11 10:50:45 +0200

    Applied code review fixes

--- highcharts-api/highcharts/2-polar-rings/main.js
+++ highcharts-api/highcharts/2-polar-rings/main.js
@@ -1,9 +1,9 @@
 const generateRandomSeriesData = () =>
   Array.from({ length: 3 }, () => Math.floor(Math.random() * 10));
 
-const chart = Highcharts.chart("container", {
+const chart = Highcharts.chart('container', {
   chart: {
-    type: "column",
+    type: 'column',
     polar: true,
     events: {
       load() {
@@ -11,96 +11,89 @@ const chart = Highcharts.chart("container", {
           dataMax = yAxis.dataMax;
 
         yAxis.update({
-          max: dataMax * 2
+          max: dataMax * 2,
         });
 
         yAxis.addPlotLine({
-          dashStyle: "Dash",
+          dashStyle: 'Dash',
           value: dataMax * 1.5,
           width: 2
         });
-
-        const redCircle = this.renderer
-          .circle(
-            this.plotWidth / 2 + this.plotLeft,
-            this.plotHeight / 2 + this.plotTop,
-            100
-          )
-          .attr({
-            fill: "white",
-            stroke: "red",
-            "stroke-width": 2
-          })
-          .add();
-
-        this.redCircle = redCircle;
       },
       render() {
-        const redCircle = this.redCircle;
+        const chart = this,
+          yAxis = chart.yAxis[0],
+          redCircle = chart.redCircle;
 
         if (redCircle) {
           redCircle.attr({
-            cx: this.plotWidth / 2 + this.plotLeft,
-            cy: this.plotHeight / 2 + this.plotTop
+            cx: chart.plotWidth / 2 + chart.plotLeft,
+            cy: chart.plotHeight / 2 + chart.plotTop,
+            r: yAxis.toPixels(
+              Math.random() * yAxis.dataMax * 2,
+              true
+            )
           });
+        } else {
+          const renderedRedCircle = chart.renderer
+            .circle(
+              chart.plotWidth / 2 + chart.plotLeft,
+              chart.plotHeight / 2 + chart.plotTop,
+              100
+            )
+            .attr({
+              fill: 'white',
+              stroke: 'red',
+              'stroke-width': 2
+            })
+            .add();
+
+          chart.redCircle = renderedRedCircle;
         }
-      }
-    }
+      },
+    },
   },
   title: {
-    text: undefined
-  },
-  pane: {
-    startAngle: 0,
-    endAngle: 360
+    text: undefined,
   },
   xAxis: {
-    categories: ["Jan", "Feb", "Mar"]
+    categories: ['Jan', 'Feb', 'Mar']
   },
   yAxis: {
-    min: 0,
-    endOnTick: false,
-    title: {
-      text: undefined
-    },
     plotLines: [
       {
         width: 2,
-        color: "green",
-        value: 15,
-        dashStyle: "solid"
+        color: 'green',
+        value: 15
       }
     ]
   },
-  credits: {
-    enabled: false
-  },
   series: [
     {
-      name: "Tokyo",
+      name: 'Tokyo',
       data: generateRandomSeriesData()
     },
     {
-      name: "New York",
+      name: 'New York',
       data: generateRandomSeriesData()
     },
     {
-      name: "London",
+      name: 'London',
       data: generateRandomSeriesData()
-    }
+    },
   ],
   plotOptions: {
     series: {
       dataLabels: {
         enabled: true,
         formatter() {
-          return this.y === this.series.yAxis.dataMax ? "max" : "";
+          return this.y === this.series.yAxis.dataMax ? 'max' : '';
         }
       }
     },
     column: {
       pointPadding: 0,
-      groupPadding: 0
+      groupPadding: 0,
     }
   }
 });
