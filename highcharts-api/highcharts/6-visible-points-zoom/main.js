d34621047bad11813637a4e938211de3c90586ac
Kamil Kubik <kamilkubik@Kamils-MBP.localdomain>
2022-10-11 15:46:23 +0200

    Applies main.js file code refactor

--- highcharts-api/highcharts/6-visible-points-zoom/main.js
+++ highcharts-api/highcharts/6-visible-points-zoom/main.js
@@ -1,5 +1,7 @@
-const generateSeries = () =>
-  Array.from({ length: 100 }, () => Math.floor(Math.random() * 99) + 1);
+const generatedSeries = Array.from(
+  { length: 100 },
+  () => Math.floor(Math.random() * 99) + 1
+);
 
 Highcharts.chart("container", {
   chart: {
@@ -21,9 +23,9 @@ Highcharts.chart("container", {
         } else {
           const renderedCountLabel = chart.renderer
             .text(
-              "Visible count: 100",
+              `Visible count: ${generatedSeries.length}`,
               chart.plotLeft,
-              chart.plotHeight + chart.plotTop + 52
+              chart.plotHeight + chart.plotTop + 50
             )
             .add();
 
@@ -86,7 +88,7 @@ Highcharts.chart("container", {
   series: [
     {
       name: "Series 1",
-      data: generateSeries(),
+      data: generatedSeries
     }
   ]
 });
