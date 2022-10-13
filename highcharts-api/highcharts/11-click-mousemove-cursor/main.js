3c7bf18bd2ef88a92ac995e74f80d772c673fc61
Kamil Kubik <kamilkubik@Kamils-MBP.localdomain>
2022-10-13 14:52:53 +0200

    Applied code refactor

--- highcharts-api/highcharts/11-click-mousemove-cursor/main.js
+++ highcharts-api/highcharts/11-click-mousemove-cursor/main.js
@@ -4,18 +4,19 @@ Highcharts.chart('container', {
     },
     series: [{
         name: 'Series 1',
-        data: [200, 150, 130, 210, 190, 170, 200, 120],
+        data: [200, 150, 130, 210, 190, 170, 200, 120]
     }]
-}, ({ container, renderer }) => {
-    const cursorCircle = renderer.circle(-50, -50, 5).attr({ fill: 'blue' }).add();
+    }, ({ container, renderer }) => {
+        const cursorCircle = renderer.circle(-50, -50, 5).attr({ fill: 'blue' }).add();
 
-    if (container) {
-        container.addEventListener('click', ({ chartX, chartY }) => {
-            renderer.circle(chartX, chartY, 5).attr({ fill: 'blue' }).add()
-        });
-    
-        container.addEventListener('mousemove', ({ chartX, chartY }) => {
-            cursorCircle.attr({ x: chartX, y: chartY });
-        });
+        if (container) {
+            container.addEventListener('click', ({ chartX, chartY }) => {
+                renderer.circle(chartX, chartY, 5).attr({ fill: 'blue' }).add()
+            });
+        
+            container.addEventListener('mousemove', ({ chartX, chartY }) => {
+                cursorCircle.attr({ x: chartX, y: chartY });
+            });
+        }
     }
-});
+);
