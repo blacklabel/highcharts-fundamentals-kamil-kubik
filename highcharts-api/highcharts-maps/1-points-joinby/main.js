6309c877021979b53185e8192cb623480fbd8953
Kamil Kubik <kamilkubik@Kamils-MBP.localdomain>
2022-10-20 14:21:15 +0200

    Applied finished maps exercise 1 version

--- highcharts-api/highcharts-maps/1-points-joinby/main.js
+++ highcharts-api/highcharts-maps/1-points-joinby/main.js
@@ -1,14 +1,23 @@
 Highcharts.mapChart('container', {
-
+    chart: {
+        map: Highcharts.maps["custom/world"]
+    },
     series: [{
+        type: 'map',
         data: [
-            ['POL', 100],
-            ['USA', 90],
-            ['PER', 50],
-            ['TZA', 40],
-            ['AUS', 1]
-        ],
-        joinBy: ['iso-a3']
+            ['pl', 1],
+            ['us', 2],
+            ['pe', 3],
+            ['tz', 4],
+            ['au', 5]
+        ]
+    }, {
+        type: 'mapbubble',
+        data: [
+            ['gl', 1],
+            ['in', 2],
+            ['gb', 3],
+            ['ao', 4]
+        ]
     }]
-
 });
