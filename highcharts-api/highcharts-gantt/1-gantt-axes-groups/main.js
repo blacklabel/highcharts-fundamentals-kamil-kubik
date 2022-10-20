4f118a49fd3d9abde86f114c51e2b65cc61220b8
Kamil Kubik <kamilkubik@Kamils-MBP.localdomain>
2022-10-20 14:59:44 +0200

    Applied gantt exercise 1 finished version

--- highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
+++ highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
@@ -1,5 +1,15 @@
 Highcharts.ganttChart('container', {
-
+    xAxis: [{
+        visible: false
+    }, {
+        labels: {
+            format: '{value: %A, %d %b, %Y}'
+        },
+        tickInterval: 432000000
+    }],
+    yAxis: {
+        categories: ['Main', 'First', 'Second']
+    },
     series: [{
         name: 'Project 1',
         data: [{
@@ -7,17 +17,19 @@ Highcharts.ganttChart('container', {
             pointWidth: 0
         }, {
             name: 'First',
-            start: 1560902400000,
-            end: 1561075200000,
-        }, {
-            name: 'Second',
+            y: 1,
             start: 1560902400000,
             end: 1561075200000
         }, {
             name: 'Second',
+            y: 2,
+            start: 1560902400000,
+            end: 1561075200000
+        }, {
+            name: 'Second',
+            y: 2,
             start: 1561507200000,
             end: 1561680000000
         }]
     }]
-
 });
