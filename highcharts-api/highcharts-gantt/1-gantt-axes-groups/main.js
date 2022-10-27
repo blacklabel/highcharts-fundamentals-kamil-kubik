ebd302621e35c5d27d5b857cda79c66c48ccef86
Kamil Kubik <kamilkubik@Kamils-MBP.localdomain>
2022-10-27 13:56:24 +0200

    Reback unwanted changes from gantt 1 exercise

--- highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
+++ highcharts-api/highcharts-gantt/1-gantt-axes-groups/main.js
@@ -1,15 +1,5 @@
 Highcharts.ganttChart('container', {
-    xAxis: [{
-        visible: false
-    }, {
-        labels: {
-            format: '{value: %A, %d %b, %Y}'
-        },
-        tickInterval: 432000000
-    }],
-    yAxis: {
-        categories: ['Main', 'First', 'Second']
-    },
+
     series: [{
         name: 'Project 1',
         data: [{
@@ -17,19 +7,17 @@ Highcharts.ganttChart('container', {
             pointWidth: 0
         }, {
             name: 'First',
-            y: 1,
+            start: 1560902400000,
+            end: 1561075200000,
+        }, {
+            name: 'Second',
             start: 1560902400000,
             end: 1561075200000
         }, {
             name: 'Second',
-            y: 2,
-            start: 1560902400000,
-            end: 1561075200000
-        }, {
-            name: 'Second',
-            y: 2,
             start: 1561507200000,
             end: 1561680000000
         }]
     }]
+
 });
